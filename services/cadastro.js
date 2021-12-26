6fd49509596aae6454f141d84fa45fb0c49a407e
Cibele <cibelemartins24@hotmail.com>
2021-12-25 23:43:48 -0300

    implementaçõess no html, css, js

--- services/cadastro.js
+++ services/cadastro.js
@@ -6,7 +6,6 @@ buttonPagInicial.addEventListener('click', () => {
    window.location = '../../index.html'
 })
 
-
 const cadastrar= document.querySelector("#btnCadastrar");
 const div1= document.querySelector("#coluna1");
 const div2= document.querySelector("#coluna2");
@@ -17,11 +16,70 @@ cadastrar.addEventListener('click', (e)=>{
 
   e.preventDefault()
 
-  div1.style.display= "none";
-  div2.style.display= "none";
-  div3.style.display= "none";
-  cadastroOk.style.display="flex"
+
+
+  if ($("#ruaInput").val()=="" && $("#bairroInput").val()=="" && $("#cidadeInput").val()=="" &&
+   $("#estadoInput").val() == "") {
+      alert("Preencha todos os campos anets de cadastrar");
+   }else {
+     cadastroOk.style.display= "flex";
+     div1.style.display= "none";
+     div2.style.display= "none";
+     div3.style.display= "none";
+   }
 
 
 
 })
+
+
+const cep= document.querySelector("#cepInput");
+
+    cep.addEventListener('blur', (event) => {
+
+      try {
+
+        const cepVal= $("#cepInput").val()
+
+        if (cepVal.length == "9" && cepVal.indexOf("-") == 5) {
+
+          $.ajax({
+            type: "GET",
+            url: `https://viacep.com.br/ws/${cepVal}/json/`,
+            success: function (response) {
+      
+              const ruaResult= response.logradouro
+              const bairroResult= response.bairro
+              const cidadeResult= response.localidade
+              const estadoResult= response.uf 
+
+              $("#ruaInput").val(ruaResult);
+              $("#bairroInput").val(bairroResult);
+              $("#cidadeInput").val(cidadeResult);
+              $("#estadoInput").val(estadoResult);
+            
+          }})
+  
+        } else {
+
+          throw new Error ("CEP inválido");
+
+        }
+        
+      } catch (erro) {
+        $("#erroCep").text("CEP inválido");
+        console.log(erro.message)
+        $("#ruaInput").val("");
+        $("#bairroInput").val("");
+        $("#cidadeInput").val("");
+        $("#estadoInput").val("");
+
+      }
+      
+    })
+
+  
+
+
+
+
