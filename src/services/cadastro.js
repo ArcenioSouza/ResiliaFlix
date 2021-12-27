edd48eb3cf9bb8dd54598f660496c0811cde5414
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-27 16:28:42 -0300

    Adiciona página de cadastro com css e js

---
+++ src/services/cadastro.js
@@ -0,0 +1,162 @@
+
+const buttonPagInicial = document.querySelector('#paginaInicial')
+
+buttonPagInicial.addEventListener('click', () => {
+   
+   window.location = '../../index.html'
+})
+
+const cadastrar= document.querySelector("#btnCadastrar");
+
+cadastrar.addEventListener('click', (e)=>{
+
+  const confirmSVal= $("#confirmaSInput").val()
+  const senhaVal= $("#senhaInput").val()
+  const emailVal= $("#emailInput").val()
+
+  e.preventDefault()
+
+  if ($("#ruaInput").val()=="" || $("#bairroInput").val()=="" || $("#cidadeInput").val()=="" ||
+      $("#estadoInput").val() == "" || $("#nomeInput").val()=="" || $("#emailInput").val()=="" || $("#senhaInput").val()=="" || $("#confirmaSInput").val()=="" || $("#complementoInput").val()=="" || $("#numeroInput").val()=="") {
+     
+      alert("Preencha todos os campos antes de se cadastrar.");
+    
+  }else if (senhaVal.length < 8 || confirmSVal.length != senhaVal.length || confirmSVal != senhaVal ) {
+      alert("Preencha os campos de senha corretamente.");
+  
+  } else if (emailVal.indexOf("@") < 0) {
+    
+    alert("Preencha o e-mail corretamente.");
+
+  }else {
+    $("#cadastroSucesso").css("display","flex");
+    $("#coluna1").css("display","none");
+    $("#coluna2").css("display","none");
+    $("#coluna3").css("display","none");
+
+    }
+
+})
+
+const cep= document.querySelector("#cepInput");
+
+    cep.addEventListener('blur', (event) => {
+
+      try {
+
+        const cepVal= $("#cepInput").val()
+
+        if (cepVal.length == "9" && cepVal.indexOf("-") == 5) {
+
+          $.ajax({
+            type: "GET",
+            url: `https://viacep.com.br/ws/${cepVal}/json/`,
+            success: function (response) {
+      
+              const ruaResult= response.logradouro
+              const bairroResult= response.bairro
+              const cidadeResult= response.localidade
+              const estadoResult= response.uf 
+
+              $("#ruaInput").val(ruaResult);
+              $("#bairroInput").val(bairroResult);
+              $("#cidadeInput").val(cidadeResult);
+              $("#estadoInput").val(estadoResult);
+              $("#erroCep").text("");
+            
+          }})
+  
+        } else {
+
+          throw new Error ("CEP inválido");
+
+        }
+        
+      } catch (erro) {
+        $("#erroCep").text("CEP inválido");
+        console.log(erro.message)
+        $("#ruaInput").val("");
+        $("#bairroInput").val("");
+        $("#cidadeInput").val("");
+        $("#estadoInput").val("");
+
+      }
+      
+    })
+
+
+const emailErro= document.querySelector("#emailInput")
+
+emailErro.addEventListener('blur', ()=> {
+
+  const emailVal= $("#emailInput").val()
+  try {
+    if (emailVal.indexOf("@") > 0){
+      
+      $("#erroEmail").css("display", "none")
+
+    } else {
+      throw new Error ("E-mail inválido");
+    }
+  } catch (erro) {
+  
+      $("#erroEmail").text("E-mail inválido");
+      $("#erroEmail").css("display", "flex")
+      console.log(erro.message)
+
+  }
+})
+
+const senhaInput= document.querySelector("#senhaInput")
+
+senhaInput.addEventListener('blur', () => {
+
+  const senhaVal= $("#senhaInput").val()
+ 
+
+  try {
+
+    if (senhaVal.length == 8) {
+      
+      $("#erroSenha").css("display", "none")
+
+    } else {
+      throw new Error ("Senha inválida")
+    }     
+    
+  } catch(erro) {
+    $("#erroSenha").text("Deve conter 8 caracteres");
+    $("#erroSenha").css("display", "flex")
+    console.log(erro.message)
+
+  }
+
+})
+
+
+const confirmSenha= document.querySelector("#confirmaSInput")
+
+confirmSenha.addEventListener('blur', () => {
+
+  const confirmSVal= $("#confirmaSInput").val()
+  const senhaVal= $("#senhaInput").val()
+
+  try {
+
+    if (confirmSVal.length == senhaVal.length && confirmSVal == senhaVal) {
+      
+      $("#erroSenha1").css("display", "none")
+
+    } else {
+      throw new Error ("Senha inválida")
+    }     
+    
+  } catch(erro) {
+    $("#erroSenha1").text("Senha inválida");
+    $("#erroSenha1").css("display", "flex")
+    console.log(erro.message)
+
+  }
+
+})
+
