7b674d7c851b6a8890ddc623e9208b067a71d6d8
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-30 16:12:45 -0300

    Adiciona funções aos botoes do navbar das telas de login e recupSenha

--- src/services/login.js
+++ src/services/login.js
@@ -35,6 +35,17 @@ entrarSite.addEventListener('click', function () {
 
 })
 
+const buttonPagInicial = document.querySelector("#pagina-inicial");
+const buttonContatos = document.querySelector("#contatos");
+
+buttonPagInicial.addEventListener("click", () => {
+   window.location = "../../index.html";
+});
+
+buttonContatos.addEventListener("click", () => {
+  window.location = "../pages/contatos.html";
+});
+
 
   
  
